3320005dadbbe842a60a695daf25e389a6d9ff5a
JetJadeja <jetjadeja@gmail.com>
2022-06-08 15:17:31 -0700

    Switch to dark mode

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,7 +1,7 @@
 const { description } = require("../../package");
 
 module.exports = {
-  title: "Huff Documentation",
+  title: "Huff Language",
   description: description,
   homepage: "http://localhost:3000/",
 
@@ -14,10 +14,12 @@ module.exports = {
     ],
   ],
 
+  theme: "default-prefers-color-scheme",
   themeConfig: {
+    overrideTheme: "dark",
     docsDir: "/",
     nav: [
-      { text: "Home", link: "/" },
+      { text: "Home", link: "/home/" },
       { text: "Get Started", link: "/get-started/" },
       { text: "Tutorial", link: "/tutorial/overview/" },
     ],
---
+++ src/home/README.md
@@ -0,0 +1,5 @@
+<script language="javascript">
+
+window.location.href = "http://localhost:3000"
+
+</script>
